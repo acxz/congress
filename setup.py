38a8313728d7c647f4f9a4a79a79e5b8c0027ea9
acxz <17132214+acxz@users.noreply.github.com>
2020-11-01 09:45:56 -0500

    add setup.py file to specify deps as well as run the main  script everywhere

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""Setup file for using congress as a python package."""
+from os import path
+
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='united-states-congress',
+    version='0.0.1',
+    author='Sunlight Foundation',
+    long_description='long_description',
+    description='Public domain data collectors for the work of Congress, '
+    'including legislation, amendments, and votes.',
+    license='CC0-1.0',
+    install_requires=[
+        'beautifulsoup4',
+        'cssselect',
+        'iso8601',
+        'lxml',
+        'mechanize',
+        'mock',
+        'rtyaml',
+        'python-dateutil',
+        'pytz',
+        'pyyaml',
+        'scrapelib',
+        'xmltodict',
+    ],
+    scripts=['run']
+)
